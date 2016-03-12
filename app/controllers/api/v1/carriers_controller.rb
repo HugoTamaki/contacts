25a0debfe6674d2f9d40988e9d69c8f1ff86106b
Hugo Tamaki <hugokt2005@yahoo.com.br>
2016-03-11 22:09:23 -0300

    fixing namespace

--- app/controllers/api/v1/carriers_controller.rb
+++ app/controllers/api/v1/carriers_controller.rb
@@ -1,5 +1,9 @@
-class CarriersController < ApplicationController
-  def index
-    @carriers = Carrier.all
+module Api
+  module V1
+    class CarriersController < ApplicationController
+      def index
+        @carriers = Carrier.all
+      end
+    end
   end
 end
